dafc23e8b58cb5597fc551c4039cd858a06e20a1
Yuval O <30690072+yuvalot@users.noreply.github.com>
2021-08-07 20:35:54 +0300

    fixed StratifiedKFold

--- src/ml_final_project/utils/hyperparameters/hyperparameter_scanner.py
+++ src/ml_final_project/utils/hyperparameters/hyperparameter_scanner.py
@@ -1,5 +1,6 @@
+import numpy as np
 from bayes_opt import BayesianOptimization
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 
 
 class HyperParameterScanner:
@@ -31,7 +32,7 @@ class HyperParameterScanner:
         kf = StratifiedKFold(n_splits=3, random_state=None, shuffle=False)
         agg = 0
 
-        for train_index, test_index in kf.split(self.X, self.y):
+        for train_index, test_index in kf.split(self.X, np.argmax(self.y, axis=1)):
             X_train, X_test = self.X[train_index], self.X[test_index]
             y_train, y_test = self.y[train_index], self.y[test_index]
             agg += runner.evaluate(X_train, X_test, y_train, y_test, self.output_dim)[0]
