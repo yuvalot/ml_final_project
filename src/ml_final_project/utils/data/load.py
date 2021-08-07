15ff6a963620dfa7dd0bfa76db8c067d99336b51
Yuval O <you@example.com>
2021-08-07 14:14:41 +0300

    adding load.py (generic dataset loader)

---
+++ src/ml_final_project/utils/data/load.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn import preprocessing
+
+
+def load_dataset(csv_path):
+    df = pd.read_csv(csv_path)
+
+    for column in df.columns:
+        if df[column].dtype == np.object:
+            df[column] = preprocessing.LabelEncoder().fit_transform(df[column].fillna('0'))
+
+    df[df.columns[-1]] = preprocessing.LabelEncoder().fit_transform(df[df.columns[-1]].fillna('0'))
+    return df
