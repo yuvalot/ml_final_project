7fa38d62879ad61e16d1e5be4bdab7f85e7fc33b
Yuval O <you@example.com>
2021-08-07 14:27:18 +0300

    adding base_runner.py (generic optimizer runner)

---
+++ src/ml_final_project/utils/runners/base_runner.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+import keras
+
+from ..evaluators.default import default_evaluator
+from ..nn.network import network
+
+
+class BaseAlgorithmRunner:
+    def __init__(self, hyper_parameters):
+        self.hyper_parameters = hyper_parameters
+
+    def _get_optimizer(self):
+        raise NotImplementedError()
+
+    def evaluate(self, X_train, X_test, y_train, y_test, output_dim, evaluator=default_evaluator):
+        model = network(input_size=X_train.shape[1], inner_dim=64, output_dim=output_dim)
+        es = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+        model.compile(optimizer=self._get_optimizer(), loss="categorical_crossentropy", metrics=["accuracy"])
+        start_training_time = datetime.now()
+        model.fit(X_train, y_train, epochs=100, verbose=0, callbacks=[es], validation_split=.2)
+        end_training_time = datetime.now()
+        return evaluator(model, X_test, y_test), (end_training_time - start_training_time).total_seconds()
